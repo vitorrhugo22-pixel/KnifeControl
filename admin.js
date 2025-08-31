91f9ea7618dce4b8a9c69cc8fe06828f374f46e8
vitorrhugo22-pixel <vitorrhugo.22@gmail.com>
2025-08-31 02:40:05 -0300

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>KnifeControl - Painel Administrativo</h1>
+      <p>Gerencie objetos, funcionários e relatórios.</p>
+    </div>
+  );
+}
---
+++ app.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>KnifeControl - Operacional</h1>
+      <p>Bem-vindo ao sistema de controle de objetos por turno.</p>
+    </div>
+  );
+}
